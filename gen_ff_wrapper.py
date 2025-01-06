fc265f93c3c6d48690f658542b15682ccb26973c
root <liang.enoch0819@gmail.com>
2025-01-06 11:24:27 +0000

    update ff_wrapper

--- gen_ff_wrapper.py
+++ gen_ff_wrapper.py
@@ -17,6 +17,8 @@ class GenFFWrapper:
 
         self.ff_value_dir = "ff_value/ff_value"
         self.ff_file_ptr = "ffi_f"
+        self.golden_value_dir = "golden_value/golden_value"
+        self.golden_file_ptr = "go_f"
 
     def print_strs(self,l:list):
         for s in l:
@@ -43,48 +45,52 @@ class GenFFWrapper:
         CNT_STR_LEN = self.cnt_str_len
         CNT_STR_WIDTH = CHAR_WIDTH * CNT_STR_LEN
         CNT_WIDTH = self.cnt_width
-
-        string = ['//=============================',
-                  '// Tasks',
-                  '//=============================']
-        string = ["task cycle2num;",
-                  f"  input [{CNT_WIDTH-1}:0] cyc;",
-                  f"  output [{CNT_STR_WIDTH-1}:0] num;",
-                  "  begin"]
+        string = f"""
+//=============================
+// Tasks
+//=============================
+task cycle2num;
+  input [{CNT_WIDTH-1}:0] cyc;
+  output [{CNT_STR_WIDTH-1}:0] num;
+  begin
+"""
         for i in range(CNT_STR_LEN-1,0,-1):
-            string = string + [f"    num2char(cyc/1{'0'*i},num[{CHAR_WIDTH*(i+1)-1}:{CHAR_WIDTH*i}]);",
-                               f"    cyc = cyc % 1{'0'*i};"]
+            string += f"    num2char(cyc/1{'0'*i},num[{CHAR_WIDTH*(i+1)-1}:{CHAR_WIDTH*i}]);"
+            string += f"    cyc = cyc % 1{'0'*i};"
 
-        string = string + ["    num2char(cyc,num[7:0]);",
-                  "  end",
-                  "endtask",
-                  "",
-                  "task num2char;",
-                  "  input [31:0] num;",
-                  "  output [7:0] ch;",
-                  "  begin",
-                  "    case(num)",
-                  "      'd0:ch=8'd48;",
-                  "      'd1:ch=8'd49;",
-                  "      'd2:ch=8'd50;",
-                  "      'd3:ch=8'd51;",
-                  "      'd4:ch=8'd52;",
-                  "      'd5:ch=8'd53;",
-                  "      'd6:ch=8'd54;",
-                  "      'd7:ch=8'd55;",
-                  "      'd8:ch=8'd56;",
-                  "      'd9:ch=8'd57;",
-                  "    endcase",
-                  "  end",
-                  "endtask"]
-        return string
+        string += f"""
+    num2char(cyc,num[7:0]);
+  end
+endtask
+
+task num2char;
+  input [31:0] num;
+  output [7:0] ch;
+  begin
+    case(num)
+      'd0:ch=8'd48;
+      'd1:ch=8'd49;
+      'd2:ch=8'd50;
+      'd3:ch=8'd51;
+      'd4:ch=8'd52;
+      'd5:ch=8'd53;
+      'd6:ch=8'd54;
+      'd7:ch=8'd55;
+      'd8:ch=8'd56;
+      'd9:ch=8'd57;
+    endcase
+  end
+endtask"""
+        print(string)
 
 
     def gen_ff_input_dump_code(self)->str:
         CLK = self.tb_clk_name
         RST = self.tb_rst_name
-        FFI = self.fault_free_input_tag_name
+        FFI = self.ff_file_ptr
         FFI_DIR = self.ff_value_dir
+        GO = self.golden_file_ptr
+        GO_DIR = self.golden_value_dir
         CNT_NAME = self.cnt_name
         CNT_STR = self.cnt_name + "_str"
         CNT_STR_LEN = self.cnt_str_len
@@ -97,51 +103,54 @@ class GenFFWrapper:
                     sig_list[cls].append("top.uut."+var)
                 
 
-        string =          [f'  reg [{CNT_STR_LEN*8-1}:0] {CNT_STR};',
-                           f'  integer {FFI};',
-                           f'  always@(posedge {CLK}) begin',
-                           f'    if({RST} && {CNT_NAME}>=0)begin',
-                           f'      if({CNT_NAME}>0)begin']
-        string = string + [f'        $fwrite({FFI},"%b\\n",{varname});' for varname in sig_list["input"]]
-        string = string + [f'        $fclose({FFI});',
-                           f'      end',
-                           f'      if({CNT_NAME}>=0)begin',
-                           f'        cycle2num({CNT_NAME},{CNT_STR});']
-        string = string + [f'        {FFI} = $fopen('+'{'+f'"{FFI_DIR}_C",{CNT_STR},".txt"'+'},"w");']
-        string = string + [f'        $fwrite(ffi_f,"%b\\n",{varname});' for varname in sig_list["ff"]]
-        string = string + [f'        $fwrite(ffi_f,"%b\\n",{varname});' for varname in sig_list["input"]]
-        string = string + [f'      end',
-                           f'    end',
-                           f'  end',
-                           ""]
+        string = f'''
+reg [{CNT_STR_LEN*8-1}:0] {CNT_STR};
+integer {FFI};
+always@(posedge {CLK}) begin
+  if({RST} && {CNT_NAME}>=0)begin
+    if({CNT_NAME}>0)begin
+'''
+        for varname in sig_list["input"]:
+            string += f'      $fwrite({FFI},"%b\\n",{varname});\n'
+        string += f'''
+      $fclose({FFI});
+    end
+    if({CNT_NAME}>=0)begin
+      cycle2num({CNT_NAME},{CNT_STR});
+      {FFI} = $fopen('+'{'+f'"{FFI_DIR}_C",{CNT_STR},".txt"'+'},"w");
+'''
+        for varname in sig_list["ff"]:
+            string += f'      $fwrite(ffi_f,"%b\\n",{varname});\n'
+        for varname in sig_list["input"]:
+            string += f'      $fwrite(ffi_f,"%b\\n",{varname});\n'
+        string += f'''
+    end
+  end
+end
+integer {GO};
+always@(posedge {CLK}) begin
+  if({RST})begin
+    if({CNT_NAME}>0)begin
+      cycle2num({CNT_NAME},{CNT_STR});
+      {GO} = $fopen({{"{GO_DIR}_C",{CNT_STR},".txt"}},"w");
+'''
+        for varname in sig_list["ff"]:
+            string += f'      $fwrite({GO},"%b\\n",{varname});\n'
+        string += f'''
+      $fclose({GO});
+    end
+  end
+end
+'''
 
-        string = string + [f'  integer go_f;',
-                           f'  always@(posedge {CLK}) begin',
-                           f'    if({RST})begin',
-                           f'      if({CNT_NAME}>0)begin',
-                           f'        cycle2num({CNT_NAME},{CNT_NAME}_num);']
-        string = string + [f'        go_f = $fopen('+'{'+f'"golden_value/Golden_Signal_Value_C",{CNT_NAME}_num,".txt"'+'},"w");']
-        string = string + [f'        $fwrite(go_f,"%b\\n",{varname});' for varname in sig_list["ff"]]
-        string = string + [f'        $fclose(ffi_f);']
-        string = string + [f'      end',
-                           f'    end',
-                           f'  end',
-                           ""]
-
-        return string
+        print( string)
 
     def generate(self):
-        print("====================================")
-        print("Start Generating Fault Free Wrapper:")
-        print("====================================")
-        string = self.gen_task()
+        self.gen_task()
         #string = string + self.gen_tb_head()
-        string = string + self.gen_cnt()
-        string = string + self.gen_ff_input_dump_code()
+        self.gen_cnt()
+        self.gen_ff_input_dump_code()
         #string = string + self.gen_tb_tail()
-
-        for s in string:
-            print(s)
 
 
 
